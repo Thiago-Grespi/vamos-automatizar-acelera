f4e81a7e62babea782e691ff4fe8439f21962335
Thiago Grespi Goulart <thiago.goulart@programmers.com.br>
2019-06-04 10:35:39 -0300

    first commit

---
+++ VamosAutomatizar/VamosAutomatizar.E2ETest/Tests/ExampleTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Xunit;
+
+namespace VamosAutomatizar.E2ETest.Tests
+{
+    public class ExampleTest
+    {
+        IWebDriver driver;
+        
+        [Fact]
+        public void GoogleSearch()
+        {
+            string dir = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+            driver = new ChromeDriver(dir);
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("https://www.google.com/");
+            IWebElement searchInput = driver.FindElement(By.Name("q"));
+            searchInput.SendKeys("Carro");
+            searchInput.SendKeys(Keys.Enter);
+            IWebElement searchResult = driver.FindElement(By.Id("resultStats"));
+            Assert.Contains("Aproximadamente", searchResult.Text);
+        }
+    }
+}
+
+
